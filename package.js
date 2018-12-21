eaefdad40dc046f7fd3041918b83e3a4a0d7897e
ItsAngel <46039429+ItsAngel@users.noreply.github.com>
2018-12-21 07:13:13 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "starly",
+  "version": "1.0.0",
+  "description": "Learn More About Pokemons!",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "ItsAngel#2846",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js": "^11.4.2",
+    "discord.js-commando": "^0.10.0",
+    "save": "^2.3.3"
+  }
+}
